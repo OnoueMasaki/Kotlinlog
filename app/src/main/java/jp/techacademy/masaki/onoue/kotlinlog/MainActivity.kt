c2300198a84cfaf8bac5a039592c4bcc34ec7d12
onouemasaki <minaseseizon0528@gmail.com>
2021-02-16 13:58:37 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/masaki/onoue/kotlinlog/MainActivity.kt
@@ -0,0 +1,20 @@
+package jp.techacademy.masaki.onoue.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val human = Human("尾上将輝", 20, "読書")
+
+        human.say()
+
+        human.think()
+
+
+    }
+}
---
+++ app/src/main/java/jp/techacademy/masaki/onoue/kotlinlog/Thinkable.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.masaki.onoue.kotlinlog
+
+interface Thinkable {
+    fun think()
+}
